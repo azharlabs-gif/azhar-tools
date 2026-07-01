493d2206add06a9f06d4fd0b0bf32aaa03cabfdb
azharlabs-gif <narejoazhar76@gmail.com>
2026-07-01 11:59:07 +0500

    Update qr-generator.js

--- tools/qr-generator.js
+++ tools/qr-generator.js
@@ -1,17 +1,1 @@
-function generateQR() {
-    const text = document.getElementById("qrText").value.trim();
-    const qr = document.getElementById("qrcode");
-
-    qr.innerHTML = "";
-
-    if (text === "") {
-        alert("Please enter text or a website link.");
-        return;
-    }
-
-    new QRCode(qr, {
-        text: text,
-        width: 220,
-        height: 220
-    });
-}
+alert("JavaScript Working!");
