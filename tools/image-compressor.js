08cc73e93776f32416cae683e9dea8cae964c57d
azharlabs-gif <narejoazhar76@gmail.com>
2026-07-03 18:30:04 +0500

    Implement image resizing in compressor
    Added functionality to resize images to a maximum width of 1000 pixels while maintaining aspect ratio.

--- tools/image-compressor.js
+++ tools/image-compressor.js
@@ -39,13 +39,25 @@ function compressImage() {
             const canvas = document.createElement("canvas");
             const ctx = canvas.getContext("2d");
 
-            canvas.width = img.width;
-            canvas.height = img.height;
+            const maxWidth = 1000;
+
+let width = img.width;
+let height = img.height;
+
+if (width > maxWidth) {
+    height = height * (maxWidth / width);
+    width = maxWidth;
+}
+
+canvas.width = width;
+canvas.height = height;
+
+ctx.drawImage(img, 0, 0, width, height);
 
             ctx.drawImage(img, 0, 0);
 
-            compressedImage = canvas.toDataURL("image/jpeg", 0.6);
-
+            compressedImage = canvas.toDataURL("image/jpeg", 0.4);
+            
             preview.src = compressedImage;
 
             downloadBtn.href = compressedImage;
